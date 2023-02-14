0b2929e337d479b0e029dcb7796c6b1fad741f21
Shrecknt <you@example.com>
2023-02-14 15:57:29 -0800

    update tests

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,16 @@
-import infixToPostfix from "infix-postfix";
-import { MutableString, Parser } from "./parser";
+import path from "path";
 import fs from "fs";
 
-let debug = false;
+import { parseProgram } from "./ast";
 
-const commands = require("../commands.json");
-const testProgram = fs.readFileSync("./test_program").toString().split("\n");
 
-type CommandParam<T> = {
-    name: string,
-    value: T
-};
+const testProgram: string = fs.readFileSync("./test_program").toString();
 
-type Command = {
-    command: string,
-    line: number,
-    params: CommandParam<any>[]
-};
-
-function parseCommand(command: string, content: MutableString): CommandParam<any>[] {
-    if (commands[command] === undefined) throw new Error(`Unknown command '${command}'`);
-    let commandArgs = commands[command] as {type: string, name: string}[];
-    let out: CommandParam<any>[] = [];
-    for (let i of commandArgs) {
-        let push: CommandParam<any> = {name: i.name, value: undefined};
-        switch (i.type) {
-            case "bool": push.value = Parser.nextBool(content); break;
-            case "char": push.value = Parser.nextChar(content); break;
-            case "float": push.value = Parser.nextFloat(content); break;
-            case "int": push.value = Parser.nextInt(content); break;
-            case "string": push.value = Parser.nextString(content); break;
-            case "word": push.value = Parser.nextWord(content); break;
-            case "remaining": push.value = Parser.allRemaining(content); break;
-            default: throw new Error("Unknown type " + i.type);
-        }
-        out.push(push);
-    }
-    return out;
-}
-
-let ast: Command[] = [];
-for (let _lineNum in testProgram) {
-    let lineNum = Number(_lineNum);
-    let line = testProgram[lineNum];
-    let str = new MutableString(line).save();
-
-    if (debug) console.log(`Line ${lineNum + 1}: ${line}`);
-
-    if (str.content.trim() === "") {
-        if (debug) console.log("Blank line");
-        continue;
-    }
-
-    let command = Parser.nextWord(str);
-    if (debug) console.log(`Command: ${command}`);
-
-    if (command === "#") {
-        if (debug) console.log(`Comment | ${Parser.allRemaining(str)}`);
-        continue;
-    }
-
-    let out = parseCommand(command, str);
-    if (debug) console.log(out);
-    ast.push({command: command, line: lineNum + 1, params: out});
-}
+let ast = parseProgram(testProgram);
 
 console.log("AST:", ast);
 
-fs.writeFileSync("ast.json", JSON.stringify(ast));
+fs.writeFileSync("ast.json", JSON.stringify(ast, null, 4));
 
 //console.log(infixToPostfix("a+b").toString().split(" "));
 
