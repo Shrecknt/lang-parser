648d0e4f67321dcd2d5d72664e821159c1404631
Shrecknt <you@example.com>
2023-02-14 17:55:59 -0800

    Remove old test code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import path from "path";
 import fs from "fs";
 
 import { parseProgram } from "./ast";
@@ -11,20 +10,3 @@ let ast = parseProgram(testProgram);
 console.log("AST:", ast);
 
 fs.writeFileSync("ast.json", JSON.stringify(ast, null, 4));
-
-//console.log(infixToPostfix("a+b").toString().split(" "));
-
-/*
-let str = new MutableString("\"testing\" test 1 true \"testing\"").save();
-console.log(str.content);
-console.log("string", Parser.nextString(str));
-console.log(str.content);
-console.log("word", Parser.nextWord(str));
-console.log(str.content);
-console.log("int", Parser.nextInt(str));
-console.log(str.content);
-console.log("bool", Parser.nextBool(str));
-console.log(str.content);
-console.log("remaining", Parser.allRemaining(str));
-console.log(str.restore().content);
-*/
